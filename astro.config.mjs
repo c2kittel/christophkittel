f844d1f78d39f57354e513aac608ae59034407ac
c2kittel <c2kittel@gmail.com>
2026-03-11 14:15:08 +0100

    Final: Switch to custom domain ckittel.eu

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
-  site: 'https://c2kittel.github.io',
-  base: '/christophkittel',
+  // Ton nouveau domaine officiel
+  site: 'https://ckittel.eu',
+  
+  // IMPORTANT : On met '/' car ton site est maintenant à la racine du domaine
+  base: '/', 
+  
   integrations: [tailwind()],
 });
