cc91cfcc47f093015ed30443178e8db129f4a88d
c2kittel <c2kittel@gmail.com>
2026-03-13 11:57:22 +0100

    CV: link towards teaching

--- src/data/cv.js
+++ src/data/cv.js
@@ -28,12 +28,41 @@ export const cvData = {
       ]
     }
   ],
+  teaching: [
+    {
+      type: "Co-Lecturer (2025 - ...)",
+      institution: "ULiège",
+      courses: [
+        { 
+          name: "Introduction to modelling in physical geography - Partim 1 : Environment", 
+          code: "GEOG2059-1", 
+          link: "https://www.programmes.uliege.be/cocoon/20252026/cours/GEOG2059-1.html" 
+        },
+        { 
+          name: "Dynamiques géographiques des enjeux du monde contemporain - Géographie de l’énergie", 
+          code: "GEOG2014-1", 
+          link: "https://www.programmes.uliege.be/cocoon/20252026/cours/GEOG2014-1.html" 
+        }
+      ]
+    },
+    {
+      type: "Guest lecturer (2025 - ...)",
+      institution: "VUB",
+      courses: [
+        { 
+          name: "Global Change - Seminar 'The oceanic circulation'", 
+          code: "1016145BNR", 
+          link: "https://caliweb.vub.be/?page=course-offer&id=006799&anchor=1&target=pr&year=2425&language=en&output=html" 
+        }
+      ]
+    }
+  ],
   education: [
     { degree: "PhD in Science", institution: "ULiège", period: "10/2016 - 03/2021" },
     { degree: "Master in Geographical Sciences (Climatology)", institution: "ULiège", period: "2014 - 2016", mention: "Summa cum laude" },
     { degree: "Bachelor in Geographical Sciences", institution: "ULiège", period: "2011 - 2014", mention: "Cum laude" }
   ],
-awards: [
+  awards: [
     { 
       title: "Prix Gérard Beltrando", 
       year: "2023", 
